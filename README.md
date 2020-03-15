adb4b8bfac6eb689b8cb7ab834d307c6ae84afa2
Yuxuan <yuxuan.xie.hit@gmail.com>
2020-03-15 11:47:53 +0100

    Yx: Solve the first one

---
+++ README.md
@@ -0,0 +1,15 @@
+# My code for practicing
+
+## Environment setting
+```shell
+  #Create gym_env for gym 
+  conda create -n gym_env -c hcc -c conda-forge python=3.6 gym
+
+  #Install for pytorch
+  conda install pytorch torchvision cudatoolkit=10.1 -c pytorchs
+
+```
+## First one -- Use DQN to solve CartPole problem 
+```shell
+python dqn_CartPole.py
+```
---
+++ dqn_CartPole.py
@@ -0,0 +1,74 @@
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+import math
+import statistics
+
+
+
+class Net(nn.Module):
+  def __init__(self):
+    super(Net, self).__init__()
+    self.fc1 = nn.Linear(4, 120) 
+    self.fc2 = nn.Linear(120, 84)
+    self.fc3 = nn.Linear(84, 2)
+
+  def forward(self, x):
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    x = self.fc3(x)
+    return x                  
+
+eps_end = 0.01
+eps_start = 1.0
+eps_decay = 100
+
+lr_end = 0.1
+lr_start = 1.0
+lr_decay = 50
+
+net = Net()
+net = net.double()
+optimizer = optim.SGD(net.parameters(), lr=0.1)
+criterion = nn.MSELoss()
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+env = gym.make('CartPole-v0')
+env.reset()
+time_setp = []
+for eposide in range(1000):
+  epsilon_ = eps_end + (eps_start - eps_end) * math.exp(-1.0 * eposide / eps_decay)
+  learning_rate = lr_end + (lr_start - lr_end) * math.exp(-1.0 * eposide / lr_decay)
+  observation = env.reset()
+  for t in range(1000):
+    env.render()
+    #Greedy action
+    output = net(torch.from_numpy(observation))
+    if random.random() < epsilon_:
+      action = env.action_space.sample()
+      Qvalue = output[action]
+    else:
+      Qvalue, action = torch.max(output,0)
+      action = action.item()
+    #
+    observation, reward, done, info = env.step(action)
+
+    #update
+    target = output.clone()
+    target[action] = Qvalue + learning_rate*(reward + 1.0* torch.max(net(torch.from_numpy(observation))) - Qvalue) 
+    
+    loss = criterion(output, target)
+    print("eposide = {}\tloss = {:.3e}\tepsilon_rate = {:.2f}\tlearning_rate = {:.2f}".format(eposide, loss.item(), epsilon_, learning_rate), end ="\r")
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if done :
+      time_setp.append(t+1)
+      print("Episode {} finished after {} timesteps and average = {:.2f}                       ".format(eposide, t+1, statistics.mean(time_setp)))
+      break
+env.close()
